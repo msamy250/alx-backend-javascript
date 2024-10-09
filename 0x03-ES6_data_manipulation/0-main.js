3decc098a46678bf51fc03f2e72c417cc3b687b1
Mahmoud Samy <mahmoudsamy@192.168.1.11>
2024-10-09 10:59:07 +0300

    update

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -1,7 +1,6 @@
 import getListStudents from "./0-get_list_students.js";
-import getStudentIdsSum from "./3-get_ids_sum.js";
+import getStudentsByLocation from "./2-get_students_by_loc.js";
 
 const students = getListStudents();
-const value = getStudentIdsSum(students);
 
-console.log(value);
+console.log(getStudentsByLocation(students, 'San Francisco'));
